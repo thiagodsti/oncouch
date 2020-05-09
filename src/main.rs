9086cf60c6682540884eb4eb41a31dbb79615881
Thiago Diniz da Silveira <thiagods.ti@gmail.com>
2020-05-09 10:07:44 +0200

    Improvement the presentation

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,15 @@ fn main() {
 
 
     let mut table = Table::new();
-    table.add_row(row!["Movie", "New Subtitle", "Old Subtitle"]);
+    table.add_row(row!["Old Subtitle", "New Subtitle"]);
     for similar in _similars.iter() {
-        let _movie = similar.movie.file_name().unwrap().to_str().unwrap();
         let _subtitle = similar.subtitle.file_name().unwrap().to_str().unwrap();
         let mut _new_subtitle = PathBuf::from(similar.subtitle.as_path());
         _new_subtitle.set_file_name(similar.movie.file_name().unwrap());
         _new_subtitle.set_extension(similar.subtitle.extension().unwrap());
 
         let new_subtitle_name = similar.movie.file_name().unwrap().to_str().unwrap().replace(similar.movie.extension().unwrap().to_str().unwrap(), similar.subtitle.extension().unwrap().to_str().unwrap());
-        table.add_row(row![_movie, new_subtitle_name, _subtitle]);
+        table.add_row(row![_subtitle, new_subtitle_name]);
     }
     table.printstd();
 
