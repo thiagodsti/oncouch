d0ae8573428e0145b8f575138a77a571b742d100
Thiago Diniz da Silveira <thiagods.ti@gmail.com>
2020-05-11 18:30:08 +0200

    Fix order to display similarities

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,16 @@ mod filemanager;
 
 pub struct SimilarFile {
     pub movie: PathBuf,
-    pub subtitle: PathBuf
+    pub subtitle: PathBuf,
+    pub chapter: Option<String>
 }
 
 impl SimilarFile {
-    pub fn new(movie: PathBuf, subtitle: PathBuf) -> Self {
+    pub fn new(movie: PathBuf, subtitle: PathBuf, chapter: Option<String>) -> Self {
         SimilarFile {
             movie,
             subtitle,
+            chapter
         }
     }
 }
@@ -30,26 +32,28 @@ pub fn find_similarities<'a>(movies: Vec<DirEntry>, subtitles: Vec<DirEntry>) ->
     let mut similarities = Vec::new();
     let mut most_similar = 0.0;
     let mut index_similar = 0;
+    let mut chapter = Option::None;
 
-    for movie in movies.iter() {
-        let movie_name = String::from(movie.file_name().to_str().unwrap());
-        let movie_chapter = find_chapter(movie_name.as_str());
-        for (i_subtitle, subtitle) in subtitles.iter().enumerate() {
-            let subtitle_name = subtitle.file_name();
-            let subtitle_name = subtitle_name.to_str().unwrap();
-            let subtitle_chapter = find_chapter(subtitle_name);
+    for subtitle in subtitles.iter() {
+        let subtitle_name = String::from(subtitle.file_name().to_str().unwrap());
+        let subtitle_chapter = find_chapter(subtitle_name.as_str());
+        for (i_movie, movie) in movies.iter().enumerate() {
+            let movie_name = movie.file_name();
+            let movie_name = movie_name.to_str().unwrap();
+            let movie_chapter = find_chapter(movie_name);
             if subtitle_chapter.is_some() && movie_chapter.is_some() && subtitle_chapter == movie_chapter {
                 most_similar = 1.0;
-                index_similar = i_subtitle;
+                index_similar = i_movie;
+                chapter = subtitle_chapter.clone();
                 continue;
             }
-            let similar = strsim::jaro(movie.file_name().to_str().unwrap(), subtitle_name);
+            let similar = strsim::jaro(subtitle.file_name().to_str().unwrap(), movie_name);
             if similar > most_similar {
                 most_similar = similar;
-                index_similar = i_subtitle;
+                index_similar = i_movie;
             }
         }
-        similarities.push(SimilarFile::new(movie.path(), subtitles[index_similar].path()));
+        similarities.push(SimilarFile::new(movies[index_similar].path(), subtitle.path(), chapter.clone()));
         most_similar = 0.0;
     }
     return similarities;
